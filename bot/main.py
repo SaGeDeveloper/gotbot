bff40b641117fc53ee42624f11f07572d6457841
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2021-06-30 20:57:42 +0300

    basics

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,13 @@
-print('avkjgrtjgtihgri')
+import telebot
+
+bot = telebot.TeleBot("1823960671:AAFfKfCcYmZATgT8FoIuNoDiIOJQ5SVgfKU")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
